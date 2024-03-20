2cabe2c4554954fe57184fb218149e45e4236726
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-03-20 21:27:37 +0800

    now activity 3 works
    still on testing it also works, needs to be change but later

--- bestal_activity1_to_5.py
+++ bestal_activity1_to_5.py
@@ -27,6 +27,7 @@ def activity1(imgfile: str) -> None:
         print("shape(height, width): ", imgInfo)
 
         # black and white show image
+        # cv2.adaptiveThreshold(...)
         (_, img) = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
         cv2.imshow("spidy image", img)
         cv2.waitKey(0)
@@ -69,18 +70,44 @@ def activity3(imgfile1: str, imgfile2: str) -> None:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        gray_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-        gray_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        histg1 = cv2.calcHist([gray_img1], [0], None, [256], [0, 256])
-        histg2 = cv2.calcHist([gray_img2], [0], None, [256], [0, 256])
-        plt.plot(histg1, color='black')
-        # plt.plot(histg2, color='red')
-        plt.xlabel('Pixel Value')
-        plt.ylabel('Frequency')
-        plt.title('Histogram of Grayscale Image')
-        plt.show()
+        (img1_b, img1_g, img1_r) = cv2.split(img1)
+        (img2_b, img2_g, img2_r) = cv2.split(img2)
+
+        img1_hist_b = cv2.calcHist([img1_b], [0], None, [256], [0, 256])
+        img1_hist_g = cv2.calcHist([img1_g], [0], None, [256], [0, 256])
+        img1_hist_r = cv2.calcHist([img1_r], [0], None, [256], [0, 256])
+
+        img2_hist_b = cv2.calcHist([img2_b], [0], None, [256], [0, 256])
+        img2_hist_g = cv2.calcHist([img2_g], [0], None, [256], [0, 256])
+        img2_hist_r = cv2.calcHist([img2_r], [0], None, [256], [0, 256])
+
+        plt.figure(figsize=(10, 5))
+        plt.plot(img1_hist_b, color="blue")
+        plt.plot(img1_hist_g, color="green")
+        plt.plot(img1_hist_r, color="red")
+
+        plt.savefig('histogram_of_image_1.png')
+        plt.close()
+
+        plt.figure(figsize=(10, 5))
+        plt.plot(img2_hist_b, color="blue")
+        plt.plot(img2_hist_g, color="green")
+        plt.plot(img2_hist_r, color="red")
+
+        plt.savefig('histogram_of_image_2.png')
+        plt.close()
+
+        img1 = cv2.imread('./histogram_of_image_1.png')
+        img2 = cv2.imread('./histogram_of_image_2.png')
+        cv2.imshow("histogram of image 1", img1)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.imshow("histogram of image 2", img2)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
-# activity1("./spidyAtDoffice.jpg")
+
+# activity1("./pic/spidyAtDoffice.jpg")
 # activity2("./spidyAtDoffice.jpg")
-activity3("./spidyAtDoffice.jpg", "./supernatural_004.png")
+activity3("./pic/spidyAtDoffice.jpg", "./pic/supernatural_004.png")
