e3bfb6f62153310a31c874612031e1972aa646a5
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-03-20 08:02:50 +0800

    testing on single pixel value and grayscale
    success on testing single pixel and turning image to grayscal

--- test.py
+++ test.py
@@ -12,3 +12,13 @@ else:
     print( "height: ", imgInfo[0] )
     print( "width: ", imgInfo[1] )
     print( "channels: ", imgInfo[2] )
+
+    print( "pixel[",int(img.shape[0]/2),"][",int(img.shape[1]/2),"]: ", img[int(img.shape[0]/2)][int(img.shape[1]/2)])
+    print( "pixel[ 0 ][ 0 ]: ", img[0][0])
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("spidy image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    imgInfo = img.shape
+    print("shape: ", imgInfo)
