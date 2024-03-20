94ae9c0d45132d45e79f47df88d4f9f5c06117e1
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-03-20 21:28:39 +0800

    testing on activity 3
    success, works fine

--- test.py
+++ test.py
@@ -6,20 +6,35 @@ plt.switch_backend('agg')
 
 img = './pic/supernatural_004.png'
 if os.path.exists(img):
-    img = cv2.imread(img)
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    histogram = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
+    (img_b, img_g, img_r) = cv2.split(cv2.imread(img))
+    print("img: ", img_b.shape)
+    print("img: ", img_g.shape)
+    print("img: ", img_r.shape)
+    hist_b = cv2.calcHist([img_b], [0], None, [256], [0, 256])
+    hist_g = cv2.calcHist([img_g], [0], None, [256], [0, 256])
+    hist_r = cv2.calcHist([img_r], [0], None, [256], [0, 256])
+    plt.figure(figsize=(10, 5))
 
-    plt.plot(histogram, color='black')
+    # plt.subplot(1, 3, 1)
+    plt.plot(hist_b, color='blue')
+
+    # plt.subplot(1, 3, 2)
+    plt.plot(hist_g, color='green')
+
+    # plt.subplot(1, 3, 3)
+    plt.plot(hist_r, color='red')
+
+    # plt.tight_layout()
+    plt.title('R(red color), G(green color), B(blue color)')
     plt.xlabel('Pixel Value')
     plt.ylabel('Frequency')
-    plt.title('Histogram of Grayscale Image')
-
-    # Save the plot as an image file
     plt.savefig('histogram.png')
-
-    # Close the plot
     plt.close()
+
+    img = cv2.imread('./histogram.png')
+    cv2.imshow("histogram", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 else:
     print("file doesn't exist")
 
