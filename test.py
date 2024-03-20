9d21f88a20bb567d525ca60e01c8ced5cbfbc6ee
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-03-20 08:08:40 +0800

    testing img file to a variable
    success, valid to have cv2.imread(imgfile), where imgfile is a string to image file

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("./spidyAtDoffice.jpg")
+imgfile = "./spidyAtDoffice.jpg"
+img = cv2.imread(imgfile)
 if img is None:
     print("image doesn't exist, or Atay, not the right dir")
 else:
