79bf0eb625a70f3ca40f90488bbe143f9cea590d
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-03-20 09:14:39 +0800

    added solution to activity 2
    success it works

--- bestal_activity1_to_5.py
+++ bestal_activity1_to_5.py
@@ -31,5 +31,31 @@ def activity1(imgfile: str) -> None:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+def activity2(imgfile: str) -> None:
+    img = cv2.imread(imgfile)
+    if img is None:
+        print("image doesn't exist, or not the right dir")
+    else:
+        # size of an image
+        imgInfo = img.shape
+        print( "size of an image")
+        print( "height: ", imgInfo[0] )
+        print( "width: ", imgInfo[1] )
 
-activity1("./spidyAtDoffice.jpg")
+        # pixel sample of an image
+        print("pixel[2][2]( B, G, R): ", img[2][2])
+
+        # edges
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        (_, img) = cv2.threshold(img, 80, 255, cv2.THRESH_BINARY)
+        cv2.imshow("spidy image", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        print("pixel[44][44]( B, G, R): ", img[44][44])
+        print("pixel[55][55]( B, G, R): ", img[55][55])
+
+
+
+# activity1("./spidyAtDoffice.jpg")
+# activity2("./spidyAtDoffice.jpg")
+# activity2("./supernatural_004.png")
