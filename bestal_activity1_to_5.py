3dabba871922d6d62882a2f48edba18bdd15c2c2
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-03-20 08:44:54 +0800

    activity 1 done
    success on testing for activity 1, here will just handle one image file

--- bestal_activity1_to_5.py
+++ bestal_activity1_to_5.py
@@ -5,6 +5,7 @@ def activity1(imgfile: str) -> None:
     if img is None:
         print("image doesn't exist, or not the right dir")
     else:
+        # RGB show image
         cv2.imshow("spidy image", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
@@ -13,8 +14,10 @@ def activity1(imgfile: str) -> None:
         print( "width: ", imgInfo[1] )
         print( "channels: ", imgInfo[2] )
         
+        # RGB show pixel
         print( "pixel[ 0 ][ 0 ]( B, G, R ): ", img[0][0])
         
+        # grayscale show image
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         cv2.imshow("spidy image", img)
         cv2.waitKey(0)
@@ -22,3 +25,11 @@ def activity1(imgfile: str) -> None:
         imgInfo = img.shape
         print("shape(height, width): ", imgInfo)
 
+        # black and white show image
+        (_, img) = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        cv2.imshow("spidy image", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+activity1("./spidyAtDoffice.jpg")
