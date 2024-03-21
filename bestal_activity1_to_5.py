59e3c81531d0d4650e66033ae30ace62c7ec8473
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-03-21 11:44:50 +0800

    modified activity 3
    due mandated to use an algo on making image black and white( ie. binary image ) not just the simple threshold, where 0 to something is 0, then above something to 255 is 255

--- bestal_activity1_to_5.py
+++ bestal_activity1_to_5.py
@@ -203,7 +203,8 @@ def activity5(imgfile: str) -> None:
 
         # black and white show image
         # cv2.adaptiveThreshold(...)
-        (_, img) = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        # (_, img) = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 4)
         cv2.imshow("spidy image", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
